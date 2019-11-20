4deb12a6f636bb873c70a7dfe19975b44308e81e
vitor-siqueira <vitorsiqueria@gmail.com>
2019-11-19 23:04:32 -0300

    String code formatting

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+.code{
+  background: #1b1d23;
+  color: rgb(175, 97, 97);
+  margin: 20px auto 20px auto;
+  padding: 15px;
+  border-radius: 5px;
+  text-align: left;
+  word-wrap: break-word;
+}
 </style>
--- src/components/gitHubIssue.vue
+++ src/components/gitHubIssue.vue
@@ -6,7 +6,9 @@
     <div v-if="!loader.getIssue" class="container">
       <h1>Issue #{{issue.number}}</h1>
       <h3>{{issue.title}}</h3>
-      <p>{{issue.body}}</p>
+
+      <pre v-html="codeText">{{codeText}}</pre>
+
       <a href="javascript:history.go(-1)" class="btn btn-success">Voltar</a>
     </div>
   </div>
@@ -22,6 +24,7 @@ export default {
   data() {
     return {
       issue: {},
+      codeText: "",
       notFind: false,
       loader: {
         getIssue: false
@@ -40,7 +43,21 @@ export default {
         .finally(() => {
           this.loader.getIssue = false;
           this.notFind = false;
+          this.formatCode();
         });
+    },
+    formatCode() {
+      let text = this.issue.body;
+      const regx = /```[^```]*```/g;
+      let regMatch = text.match(regx);
+      if (regMatch) {
+        //replace "```" to HTML tag and replace the end "`," of each code regex and close the tag
+        this.codeText = text.replace(
+          regx,
+          `<div class='code'>${regMatch}</div>`.replace(/`,/g,`</div><div class='code'>`)).replace(/`/g, '')
+      } else {
+        this.codeText = text;
+      }
     }
   }
 };
@@ -57,5 +74,22 @@ export default {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+}
+.btn {
+  position: absolute;
+  left: 45px;
+  top: 45px;
+}
+pre {
+  display: block;
+  font-size: 87.5%;
+  color: #212529;
+  width: 100%;
+  margin: 0;
+  padding: 20px;
+  position: absolute;
+  left: 0;
+  text-align: left;
+  border-top: 1px solid #aaa;
 }
 </style>
